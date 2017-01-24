116f26f6d53eec94d9c8277c721b7228bc3837ba
Zachary Burns <developer@zackburns.com>
2017-01-24 13:43:18 -0500

    Add files via upload

---
+++ IosAndroidAppIconGenerator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace IosAndroidAppIconGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 271);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Choose Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Generate Splashes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Splash Screen Image";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(305, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "App Icon Image";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(441, 320);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Generate Icons";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(305, 320);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "&Choose Image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(305, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(266, 271);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 357);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iOS and Android Icon Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ IosAndroidAppIconGenerator/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IosAndroidAppIconGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string splash = String.Empty;
+        string icon = String.Empty;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.DefaultExt = ".png";
+            of.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                splash = of.FileName;
+                pictureBox1.Image = System.Drawing.Bitmap.FromFile(of.FileName);
+            }
+            else
+            {
+                splash = String.Empty;
+                pictureBox1.Image = null;
+            }
+            of.Dispose();
+            of = null;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.DefaultExt = ".png";
+            of.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                icon = of.FileName;
+                pictureBox2.Image = System.Drawing.Bitmap.FromFile(of.FileName);
+            }
+            else
+            {
+                icon = String.Empty;
+                pictureBox2.Image = null;
+            }
+            of.Dispose();
+            of = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //generate splashes - iPhone
+            FixedSize(pictureBox1.Image, 320, 480).Save("iPhone_Launch_Image_320_480.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 640, 960).Save("iPhone_Launch_Image_640_960.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 640, 1136).Save("iPhone_Launch_Image_640_1136.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 750, 1334).Save("iPhone_Launch_Image_750_1334.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 1242, 2208).Save("iPhone_Launch_Image_1242_2208.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 2208, 1242).Save("iPhone_Launch_Image_2208_1242.png", System.Drawing.Imaging.ImageFormat.Png);
+            //generate splashes - Android
+            FixedSize(pictureBox1.Image, 426, 320).Save("Android_Splash_Image_426_320.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 470, 320).Save("Android_Splash_Image_470_320.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 640, 480).Save("Android_Splash_Image_640_480.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 960, 720).Save("Android_Splash_Image_960_720.png", System.Drawing.Imaging.ImageFormat.Png);
+            //generate splashes - iPad
+            FixedSize(pictureBox1.Image, 768, 1004).Save("iPad_Launch_Image_768_1004.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 768, 1024).Save("iPad_Launch_Image_768_1024.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 1024, 748).Save("iPad_Launch_Image_1024_748.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 1024, 768).Save("iPad_Launch_Image_1024_768.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 1536, 2008).Save("iPad_Launch_Image_1536_2008.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 1536, 2048).Save("iPad_Launch_Image_1536_2048.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 2048, 1496).Save("iPad_Launch_Image_2048_1496.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox1.Image, 2048, 1536).Save("iPad_Launch_Image_2048_1536.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private static Image FixedSize(Image imgPhoto, int Width, int Height)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((Width - (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((Height - (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(Width, Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            grPhoto.Clear(Color.FromArgb(0, 0, 0, 0));
+            grPhoto.DrawImage(imgPhoto, new Rectangle(destX, destY, destWidth, destHeight), new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight), GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //generate application icons
+            FixedSize(pictureBox2.Image, 57, 57).Save("iPhone_Application_Icon_57_57.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 60, 60).Save("iPhone_Application_Icon_60_60.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 87, 87).Save("iPhone_Application_Icon_87_87.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 114, 114).Save("iPhone_Application_Icon_114_114.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 120, 120).Save("iPhone_Application_Icon_120_120.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 180, 180).Save("iPhone_Application_Icon_180_180.png", System.Drawing.Imaging.ImageFormat.Png);
+            //generate spotlight search icons
+            FixedSize(pictureBox2.Image, 29, 29).Save("iPhone_Spotlight_Icon_29_29.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 40, 40).Save("iPhone_Spotlight_Icon_40_40.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 58, 58).Save("iPhone_Spotlight_Icon_58_58.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 80, 80).Save("iPhone_Spotlight_Icon_80_80.png", System.Drawing.Imaging.ImageFormat.Png);
+            //generate android launch icons
+            FixedSize(pictureBox2.Image, 36, 36).Save("Android_Launcher_Icon_36_36.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 48, 48).Save("Android_Launcher_Icon_48_48.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 72, 72).Save("Android_Launcher_Icon_72_72.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 96, 96).Save("Android_Launcher_Icon_96_96.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 144, 144).Save("Android_Launcher_Icon_144_144.png", System.Drawing.Imaging.ImageFormat.Png);
+            //generate iPad application icons
+            FixedSize(pictureBox2.Image, 72, 72).Save("iPad_Application_Icon_72_72.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 76, 76).Save("iPad_Application_Icon_76_76.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 144, 144).Save("iPad_Application_Icon_144_144.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 152, 152).Save("iPad_Application_Icon_152_152.png", System.Drawing.Imaging.ImageFormat.Png);
+            //generate iPad spotlight search icons
+            FixedSize(pictureBox2.Image, 40, 40).Save("iPad_Spotlight_Icon_40_40.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 50, 50).Save("iPad_Spotlight_Icon_50_50.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 80, 80).Save("iPad_Spotlight_Icon_80_80.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 100, 100).Save("iPad_Spotlight_Icon_100_100.png", System.Drawing.Imaging.ImageFormat.Png);
+            //generate iPad spotlight search icons
+            FixedSize(pictureBox2.Image, 29, 29).Save("iPad_Setting_Icon_29_29.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 58, 58).Save("iPad_Setting_Icon_58_58.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            //generate website icons for Apple Self Hosted Enterprise Applications
+            FixedSize(pictureBox2.Image, 57, 57).Save("Website_Icon_57_57.png", System.Drawing.Imaging.ImageFormat.Png);
+            FixedSize(pictureBox2.Image, 512, 512).Save("Website_Icon_512_512.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            //generate traditional windows 32x32, etc icon
+            FixedSize(pictureBox2.Image, 16, 16).Save("Microsoft_Windows_Icon_16_16.ico", System.Drawing.Imaging.ImageFormat.Icon);
+            FixedSize(pictureBox2.Image, 24, 24).Save("Microsoft_Windows_Icon_24_24.ico", System.Drawing.Imaging.ImageFormat.Icon);
+            FixedSize(pictureBox2.Image, 32, 32).Save("Microsoft_Windows_Icon_32_32.ico", System.Drawing.Imaging.ImageFormat.Icon);
+            FixedSize(pictureBox2.Image, 96, 96).Save("Microsoft_Windows_Icon_96_96.ico", System.Drawing.Imaging.ImageFormat.Icon);
+        }
+    }
+}
